85b327ef55695c112f2c16fbd930e4b6eb62bb1d
Josh van Eyken <josh.vaneyken@gmail.com>
2023-03-01 11:05:29 -0800

    begin user class

---
+++ users.rb
@@ -0,0 +1,25 @@
+require 'sqlite3'
+require_relative 'questions_database.rb'
+
+class Users
+    def self.find_by_id(id)
+        user = QuestionsDatabase.instance.execute(<<-SQL, id)
+            SELECT
+                *
+            FROM
+                users
+            WHERE
+                id = ?
+        SQL
+        return nil unless user.length > 0
+        Users.new(user.first)
+    end
+
+    def initialize(options)
+        @id = options['id']
+        @fname = options['fname']
+        @lname = options['lname']
+    end
+
+end
+
